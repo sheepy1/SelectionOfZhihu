8d209b3eeae4d63b60575475285a8a5161cc043c
杨洋 <Sheepy@yangyangdeMacBook-Pro.local>
2016-01-18 11:35:23 +0800

    Remove SwiftyJSON

--- SelectionOfZhihu/UserCell.swift
+++ SelectionOfZhihu/UserCell.swift
@@ -6,62 +6,62 @@
 //  Copyright © 2016年 Sheepy. All rights reserved.
 //
 
-import UIKit
+//import UIKit
+//
+//class UserCell: UITableViewCell {
+//    var avatarImageView: UIImageView!
+//    
+//    var nameLabel: UILabel!
+//    
+//    var signatureLabel: UILabel!
+//    
+//    var followImageView: UIImageView!
+//    
+//    var followCount: UILabel!
+//    
+//    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        
+//        avatarImageView = UIImageView(frame: CGRect(x: 8, y: 8, width: 80, height: 80))
+//        
+//        nameLabel = UILabel(frame: CGRect(x: 100, y: 8, width: 200, height: 30))
+//        
+//        signatureLabel = UILabel(frame: CGRect(x: 100, y: 40, width: 300, height: 30))
+//        
+//        followImageView = UIImageView(frame: CGRect(x: 300, y: 8, width: 10, height: 10))
+//        followImageView.image = UIImage(named: "agree")
+//        
+//        followCount = UILabel(frame: CGRect(x: 320, y: 8, width: 50, height: 20))
+//        
+//        [avatarImageView, nameLabel, signatureLabel, followImageView, followCount].forEach {
+//            addSubview($0)
+//        }
+//    }
+//    
+//    func bindModel(model: TopUserModel, withIndex index: Int) {
+//        avatarImageView.setImageWithId(index, imagePath: model.avatar)
+//        
+//        nameLabel.text = model.name
+//        
+//        signatureLabel.text = model.signature
+//        
+//        followCount.text = "\(model.agree)"
+//    }
+//
+//    func bindModel(model: JSON, withIndex index: Int) {
+//        avatarImageView.setImageWithId(index, imagePath: model["avatar"].stringValue)
+//        
+//        nameLabel.text = model["name"].stringValue
+//        
+//        signatureLabel.text = model["signature"].stringValue
+//        
+//        followCount.text = "\(model["agree"].intValue)"
+//    }
 
-class UserCell: UITableViewCell {
-    var avatarImageView: UIImageView!
-    
-    var nameLabel: UILabel!
-    
-    var signatureLabel: UILabel!
-    
-    var followImageView: UIImageView!
-    
-    var followCount: UILabel!
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        avatarImageView = UIImageView(frame: CGRect(x: 8, y: 8, width: 80, height: 80))
-        
-        nameLabel = UILabel(frame: CGRect(x: 100, y: 8, width: 200, height: 30))
-        
-        signatureLabel = UILabel(frame: CGRect(x: 100, y: 40, width: 300, height: 30))
-        
-        followImageView = UIImageView(frame: CGRect(x: 300, y: 8, width: 10, height: 10))
-        followImageView.image = UIImage(named: "agree")
-        
-        followCount = UILabel(frame: CGRect(x: 320, y: 8, width: 50, height: 20))
-        
-        [avatarImageView, nameLabel, signatureLabel, followImageView, followCount].forEach {
-            addSubview($0)
-        }
-    }
-    
-    func bindModel(model: TopUserModel, withIndex index: Int) {
-        avatarImageView.setImageWithId(index, imagePath: model.avatar)
-        
-        nameLabel.text = model.name
-        
-        signatureLabel.text = model.signature
-        
-        followCount.text = "\(model.agree)"
-    }
-    
-    func bindModel(model: JSON, withIndex index: Int) {
-        avatarImageView.setImageWithId(index, imagePath: model["avatar"].stringValue)
-        
-        nameLabel.text = model["name"].stringValue
-        
-        signatureLabel.text = model["signature"].stringValue
-        
-        followCount.text = "\(model["agree"].intValue)"
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-    
-}
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    
+//    
+//}
